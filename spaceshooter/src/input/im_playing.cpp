9276939a6e53ba042536a3943571da3c985722a0
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-13 13:42:51 +0900

    feat: 長押しで攻撃アクションを生成するように変更。

--- spaceshooter/src/input/im_playing.cpp
+++ spaceshooter/src/input/im_playing.cpp
@@ -29,7 +29,7 @@ std::vector<InputAction> IM_Playing::GenerateInputAction() {
     }
 
     // 攻撃アクション
-    if (key_state_[SDL_SCANCODE_SPACE] == 1) {
+    if (key_state_[SDL_SCANCODE_SPACE] > 0) {
         auto action = InputAction{kFire};
         action.b_value = true;
         actions.push_back(action);
