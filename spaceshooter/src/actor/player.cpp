181222263c96bc2fe9c7606281f8dc5f3caa07c3
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-13 13:59:12 +0900

    feat: PlayerにWeaponを追加、他のメンバー変数の型を変更。

--- spaceshooter/src/actor/player.cpp
+++ spaceshooter/src/actor/player.cpp
@@ -4,47 +4,56 @@
 #include <stdexcept>
 #include <typeinfo>
 
+#include "machine_gun.h"
+
 namespace spaceshooter {
 
-Player::Player(Texture* texture, float width, float height, float pos_x, float pos_y, float speed)
-    : texture_(NULL), width_(width), height_(height), pos_x_(pos_x), pos_y_(pos_y), speed_(speed) {
+Player::Player(Texture* texture, Vector2 pos, Vector2 size, float speed)
+    : texture_(NULL), pos_(pos), size_(size), speed_(speed) {
     texture_ = texture->sdl();
+    weapon_ = new MachineGun(Vector2::zero, 0.1f);
 }
 
 Player::~Player() {
     texture_ = NULL;
 
-    width_ = 0.f;
-    height_ = 0.f;
-    pos_x_ = 0.f;
-    pos_y_ = 0.f;
-    speed_ = 0.f;
+    if (weapon_ != NULL) {
+        delete weapon_;
+        weapon_ = NULL;
+    }
 }
 
 void Player::Move(Vector2 direction, Range x_limit, Range y_limit, float delta_time) {
-    pos_x_ += direction.x * speed_ * delta_time;
-    pos_y_ += direction.y * speed_ * delta_time;
+    pos_ += direction * speed_ * delta_time;
 
     // 画面外に出ないように位置を補正
-    if (pos_x_ < 0.f) {
-        pos_x_ = 0.f;
-    } else if (pos_x_ >= x_limit.max - width_) {
-        pos_x_ = x_limit.max - width_;
+    if (pos_.x < 0.f) {
+        pos_.x = 0.f;
+    } else if (pos_.x >= x_limit.max - size_.x) {
+        pos_.x = x_limit.max - size_.x;
     }
-    if (pos_y_ < 0.f) {
-        pos_y_ = 0.f;
-    } else if (pos_y_ >= y_limit.max - height_) {
-        pos_y_ = y_limit.max - height_;
+    if (pos_.y < 0.f) {
+        pos_.y = 0.f;
+    } else if (pos_.y >= y_limit.max - size_.y) {
+        pos_.y = y_limit.max - size_.y;
     }
+
+    weapon_->set_pos(pos_ + size_ / 2.f);
 }
 
 void Player::Render(SDL_Renderer* renderer) {
     SDL_FRect rect;
-    rect.x = pos_x_;
-    rect.y = pos_y_;
-    rect.w = width_;
-    rect.h = height_;
+    rect.x = pos_.x;
+    rect.y = pos_.y;
+    rect.w = size_.x;
+    rect.h = size_.y;
     SDL_RenderCopyF(renderer, texture_, NULL, &rect);
 }
+
+bool Player::CanFire() { return weapon_->CanFire(); }
+
+void Player::CountdownFiringInterval(float delta_time) { weapon_->CountdownInterval(delta_time); }
+
+Bullet* Player::Fire() { return weapon_->Fire(); }
 
 } // namespace spaceshooter
--- spaceshooter/src/actor/player.h
+++ spaceshooter/src/actor/player.h
@@ -5,8 +5,11 @@
 #include <vector>
 
 #include "../common/range.h"
+#include "../common/vector2.h"
 #include "../input/input_action.h"
 #include "../texture/texture.h"
+#include "bullet.h"
+#include "weapon.h"
 
 namespace spaceshooter {
 
@@ -14,15 +17,15 @@ class Player {
 
  private:
     SDL_Texture* texture_;
-    float width_;
-    float height_;
-    float pos_x_;
-    float pos_y_;
+    Vector2 pos_;
+    Vector2 size_;
 
     float speed_;
 
+    Weapon* weapon_;
+
  public:
-    Player(Texture*, float width = 64.f, float height = 64.f, float pos_x = 0.f, float pos_y = 0.f,
+    Player(Texture*, Vector2 pos = Vector2::zero, Vector2 size = Vector2{64.f, 64.f},
            float speed = 300);
     Player(const Player&) = delete;
 
@@ -30,6 +33,9 @@ class Player {
 
     void Move(Vector2 direction, Range x_limit, Range y_limit, float delta_time);
     void Render(SDL_Renderer* renderer);
+    bool CanFire();
+    void CountdownFiringInterval(float delta_time);
+    Bullet* Fire();
 
     Player& operator=(const Player&) = delete;
 };
