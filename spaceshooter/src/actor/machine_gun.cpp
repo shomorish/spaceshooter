edd9f5bdaea451ff604cb327df5510098b66d773
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-13 13:56:17 +0900

    feat: Weaponを継承するようにMachineGunを変更。

--- spaceshooter/src/actor/machine_gun.cpp
+++ spaceshooter/src/actor/machine_gun.cpp
@@ -4,14 +4,9 @@
 
 namespace spaceshooter {
 
-MachineGun::MachineGun(Vector2 pos, float firing_interval)
-    : pos_(pos), firing_interval_(firing_interval), interval_count_(0.f) {}
+MachineGun::MachineGun(Vector2 pos, float firing_interval) : Weapon{pos, firing_interval, 0.f} {}
 
 MachineGun::~MachineGun() {}
-
-Vector2 MachineGun::get_pos() { return pos_; }
-
-void MachineGun::set_pos(Vector2 pos) { pos_ = pos; }
 
 Bullet* MachineGun::Fire() {
     interval_count_ = firing_interval_;
@@ -19,9 +14,5 @@ Bullet* MachineGun::Fire() {
     return new StraightBullet(pos_, Vector2{8.f, 8.f}, Vector2::up, 1000.f, 3.f, true,
                               Color::yellow);
 }
-
-bool MachineGun::CanFire() { return interval_count_ <= 0.f; }
-
-void MachineGun::CountdownInterval(float delta_time) { interval_count_ -= delta_time; }
 
 } // namespace spaceshooter
--- spaceshooter/src/actor/machine_gun.h
+++ spaceshooter/src/actor/machine_gun.h
@@ -2,26 +2,17 @@
 
 #include "../common/vector2.h"
 #include "bullet.h"
+#include "weapon.h"
 
 namespace spaceshooter {
 
-class MachineGun {
+class MachineGun : public Weapon {
  public:
     MachineGun(Vector2 pos, float firing_interval);
 
     ~MachineGun();
 
-    Vector2 get_pos();
-    void set_pos(Vector2 pos);
-
-    Bullet* Fire();
-    bool CanFire();
-    void CountdownInterval(float delta_time);
-
- private:
-    Vector2 pos_;
-    float firing_interval_;
-    float interval_count_;
+    Bullet* Fire() override;
 };
 
 } // namespace spaceshooter
