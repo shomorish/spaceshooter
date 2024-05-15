1e4835e9945c4a527a1b49dba5684012c16cf3fd
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-15 16:29:46 +0900

    feat: Stage1に小惑星と背景が表示されるように変更。

--- spaceshooter/src/level/stage1.cpp
+++ spaceshooter/src/level/stage1.cpp
@@ -1,5 +1,9 @@
 #include "stage1.h"
 
+#include <random>
+
+#include "../actor/asteroid.h"
+#include "../actor/background.h"
 #include "../actor/straight_bullet.h"
 #include "../input/im_playing.h"
 
@@ -11,7 +15,29 @@ Stage1::Stage1(Window* window, Renderer* renderer, AssetManager* asset_manager,
 
     *input_mapping_ = new IM_Playing();
 
+    actors_.push_back(new Background(asset_manager->GetTexture(AssetKey::kBackground),
+                                     Vector2{-800.f, -600.f}, Vector2{1024.f * 4.f, 512.f * 4.f}));
+
     player_controller_ = new PlayerController(this);
+
+    std::random_device seed_gen;
+    std::mt19937 engine(seed_gen());
+    for (int i = 0; i < 80; i++) {
+        float scale = std::uniform_real_distribution<float>(1.f, 2.5f)(engine);
+        Vector2 size = Vector2{30.f, 30.f} * scale;
+
+        Vector2 pos =
+            Vector2{std::uniform_real_distribution<float>(-800.f, 1600.f - size.x)(engine),
+                    std::uniform_real_distribution<float>(-600.f, 1200.f - size.y)(engine)};
+
+        Texture* texture = asset_manager_->GetTexture(kSpin00);
+
+        float angle = 0.f;
+
+        float rotation_speed = std::uniform_real_distribution<float>(-180.f, 180.f)(engine);
+
+        actors_.push_back(new Asteroid(texture, pos, size, angle, rotation_speed));
+    }
 }
 
 Stage1::~Stage1() {
