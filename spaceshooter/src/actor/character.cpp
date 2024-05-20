14c9abb2c453a2b45ddb4ffa072605bd5c494aad
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-21 00:20:14 +0900

    feat: CharacterのRenderメソッドを修正。

--- spaceshooter/src/actor/character.cpp
+++ spaceshooter/src/actor/character.cpp
@@ -16,7 +16,7 @@ SDL_Texture* Character::get_texture() { return texture_; }
 void Character::set_texture(SDL_Texture* texture) { texture_ = texture; }
 
 void Character::Render(SDL_Renderer* renderer) {
-    SDL_FRect rect{pos_.x, pos_.y, size_.x, size_.y};
+    SDL_FRect rect{pos_.x - size_.x / 2.f, pos_.y - size_.y / 2.f, size_.x, size_.y};
     SDL_RenderCopyExF(renderer, texture_, NULL, &rect, angle_, NULL, SDL_FLIP_NONE);
 }
 
