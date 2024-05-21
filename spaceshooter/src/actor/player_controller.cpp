e99946ed98db577eff99093c78ceab210e7dfed8
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-21 23:37:51 +0900

    refactor: プレイヤーのテクスチャを変更。

--- spaceshooter/src/actor/player_controller.cpp
+++ spaceshooter/src/actor/player_controller.cpp
@@ -9,7 +9,7 @@ namespace spaceshooter {
 
 PlayerController::PlayerController(Level* level, Range area_x_range, Range area_y_range)
     : level_(level) {
-    Player* player = new Player(level->get_asset_manager()->GetTexture(AssetKey::kSpaceship1Blue));
+    Player* player = new Player(level->get_asset_manager()->GetTexture(AssetKey::kShip1));
     character_ = player;
     auto size = player->get_size();
     x_movable_range_ = Range{area_x_range.min + size.x / 2.f, area_x_range.max - size.x / 2.f};
