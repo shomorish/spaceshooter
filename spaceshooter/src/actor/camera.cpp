f5e40f6d1e52eb90f23b1045e7732572fe335993
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-21 12:11:39 +0900

    feat: CameraにInitメソッドを追加。

--- spaceshooter/src/actor/camera.cpp
+++ spaceshooter/src/actor/camera.cpp
@@ -2,12 +2,30 @@
 
 namespace spaceshooter {
 
-Camera::Camera() : pos_(Vector2::zero) {}
+Camera::Camera() : pos_(Vector2::zero), x_range_(Range{0.f, 0.f}), y_range_(Range{0.f, 0.f}) {}
 
 Camera::~Camera() {}
 
 Vector2 Camera::get_pos() { return pos_; }
 
-void Camera::set_pos(Vector2 pos) { pos_ = pos; }
+float Clamp(float current, float min, float max) {
+    if (current < min) {
+        current = min;
+    } else if (current > max) {
+        current = max;
+    }
+    return current;
+}
+
+void Camera::set_pos(Vector2 pos) {
+    pos.x = Clamp(pos.x, x_range_.min, x_range_.max);
+    pos.y = Clamp(pos.y, y_range_.min, y_range_.max);
+    pos_ = pos;
+}
+
+void Camera::Init(Range x_range, Range y_range) {
+    x_range_ = x_range;
+    y_range_ = y_range;
+}
 
 } // namespace spaceshooter
--- spaceshooter/src/actor/camera.h
+++ spaceshooter/src/actor/camera.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../common/vector2.h"
+#include "../common/range.h"
 
 namespace spaceshooter {
 
@@ -13,8 +14,12 @@ class Camera {
     Vector2 get_pos();
     void set_pos(Vector2 pos);
 
+    void Init(Range x_range, Range y_range);
+
  private:
     Vector2 pos_;
+    Range x_range_;
+    Range y_range_;
 };
 
 } // namespace spaceshooter
