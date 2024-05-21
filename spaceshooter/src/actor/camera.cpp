5ff2f50486c4401f7fba5cfd3909892265dc066d
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-21 17:11:29 +0900

    feat: Cameraクラスを修正。
    Initメソッドを変更。
    ConvertToWindowCoordsメソッドを追加。

--- spaceshooter/src/actor/camera.cpp
+++ spaceshooter/src/actor/camera.cpp
@@ -1,12 +1,17 @@
 #include "camera.h"
 
+#include <cfloat>
+#include <stdio.h>
+
 namespace spaceshooter {
 
-Camera::Camera() : pos_(Vector2::zero), x_range_(Range{0.f, 0.f}), y_range_(Range{0.f, 0.f}) {}
+Camera::Camera()
+    : pos_(Vector2::zero), x_range_(Range{0.f, 0.f}), y_range_(Range{0.f, 0.f}),
+      half_window_width_(0.f), half_window_height_(0.f) {}
 
 Camera::~Camera() {}
 
-Vector2 Camera::get_pos() { return pos_; }
+Vector2 Camera::get_pos() const { return pos_; }
 
 float Clamp(float current, float min, float max) {
     if (current < min) {
@@ -23,9 +28,25 @@ void Camera::set_pos(Vector2 pos) {
     pos_ = pos;
 }
 
-void Camera::Init(Range x_range, Range y_range) {
-    x_range_ = x_range;
-    y_range_ = y_range;
+void Camera::Init(Range area_x_range, Range area_y_range, Window* window) {
+    half_window_width_ = window->get_width() / 2.f;
+    half_window_height_ = window->get_height() / 2.f;
+    x_range_ = Range{area_x_range.min + half_window_width_, area_x_range.max - half_window_width_};
+    y_range_ =
+        Range{area_y_range.min + half_window_height_, area_y_range.max - half_window_height_};
+}
+
+bool Camera::ConvertToWindowCoords(Vector2 pos, Vector2* out) {
+    // if (!(x_range_.withinRange(pos.x) && y_range_.withinRange(pos.y))) {
+    // if (!((0 <= pos.x && pos.x <= 1600.f) && (0 <= pos.y && pos.y <= 1600.f))) {
+    //    *out = Vector2{FLT_MIN, FLT_MIN};
+    //    return false;
+    //}
+
+    Vector2 rel = pos - pos_;
+    Vector2 half_window_size{half_window_width_, half_window_height_};
+    *out = half_window_size + rel;
+    return true;
 }
 
 } // namespace spaceshooter
--- spaceshooter/src/actor/camera.h
+++ spaceshooter/src/actor/camera.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include "../common/vector2.h"
 #include "../common/range.h"
+#include "../common/vector2.h"
+#include "../window.h"
 
 namespace spaceshooter {
 
@@ -11,15 +12,18 @@ class Camera {
 
     ~Camera();
 
-    Vector2 get_pos();
+    Vector2 get_pos() const;
     void set_pos(Vector2 pos);
 
-    void Init(Range x_range, Range y_range);
+    void Init(Range area_x_range, Range area_y_range, Window* window);
+    bool ConvertToWindowCoords(Vector2 pos, Vector2* out);
 
  private:
     Vector2 pos_;
     Range x_range_;
     Range y_range_;
+    float half_window_width_;
+    float half_window_height_;
 };
 
 } // namespace spaceshooter
