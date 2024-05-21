d24ce45e6137f4b7f8de86d1a5b31ab3611ea592
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-22 00:22:11 +0900

    fix: Alienのコンストラクタを修正。

--- spaceshooter/src/actor/alien.cpp
+++ spaceshooter/src/actor/alien.cpp
@@ -2,8 +2,8 @@
 
 namespace spaceshooter {
 
-Alien::Alien(Texture* texture, Vector2 pos, Vector2 size = Vector2{64.f, 64.f}, float speed = 60.f,
-             Vector2 direction, float rotation_speed = 120.f, float hp = 25.f)
+Alien::Alien(Texture* texture, Vector2 pos, Vector2 size, float speed, Vector2 direction,
+             float rotation_speed, float hp)
     : Character{pos, size, direction, texture->sdl()}, speed_(speed),
       rotation_speed_(rotation_speed), hp_(hp) {}
 
