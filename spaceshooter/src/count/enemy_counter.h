d01cba5eedcf0cccf3610271a91f27a744776dc0
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-22 13:31:00 +0900

    feat: スポーンした敵の数などを管理する構造体を追加。

---
+++ spaceshooter/src/count/enemy_counter.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace spaceshooter {
+
+struct EnemyCounter {
+    int max_spawn_enemies;
+    int num_of_spawned_enemies;
+    int num_of_destroy_enemies;
+};
+
+} // namespace spaceshooter
