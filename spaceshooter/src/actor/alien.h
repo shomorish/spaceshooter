89319295d0dd8c763dc73dbc982662e0728cef86
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-22 09:56:26 +0900

    feat: Alienの移動方向の初期値を変更。

--- spaceshooter/src/actor/alien.h
+++ spaceshooter/src/actor/alien.h
@@ -8,7 +8,7 @@ namespace spaceshooter {
 class Alien : public Character {
  public:
     Alien(Texture* texture, Vector2 pos, Vector2 size = Vector2{64.f, 64.f}, float speed = 60.f,
-          Vector2 direction = Vector2::zero, float rotation_speed = 120.f, float hp = 25.f);
+          Vector2 direction = Vector2::up, float rotation_speed = 120.f, float hp = 25.f);
 
     ~Alien();
 
