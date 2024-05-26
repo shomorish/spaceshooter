e10067b1cfca0d16eae8db6d05f610d4f17cea03
shomorish <123480685+shomorish@users.noreply.github.com>
2024-05-26 18:36:19 +0900

    feat: Viewを継承するようにTextViewを変更。

--- spaceshooter/src/gui/text_view.cpp
+++ spaceshooter/src/gui/text_view.cpp
@@ -2,33 +2,16 @@
 
 namespace spaceshooter {
 
-TextView::TextView() : view_info_(ViewInfo()), font_(NULL), texture_(NULL), window_(NULL) {}
-
-TextView::TextView(Window* window, ViewInfo view_info, std::string font_path, int point_size)
-    : view_info_(view_info), font_(NULL), texture_(NULL), window_(window) {
-
-    if (!font_path.empty() && point_size != 0) {
-        set_font(font_path, point_size);
-    }
-}
+TextView::TextView() : font_(NULL) {}
 
 TextView::~TextView() {
-    window_ = NULL;
-
     if (font_) {
         delete font_;
         font_ = NULL;
     }
-
-    if (texture_) {
-        delete texture_;
-        texture_ = NULL;
-    }
 }
 
 void TextView::set_font(std::string path, int point_size) { font_ = new Font(path, point_size); }
-
-void TextView::set_window(Window* window) { window_ = window; }
 
 void TextView::SetText(std::string text, SDL_Color text_color, SDL_Renderer* renderer) {
     if (font_) {
@@ -37,86 +20,6 @@ void TextView::SetText(std::string text, SDL_Color text_color, SDL_Renderer* ren
             texture_ = NULL;
         }
         texture_ = new Texture(font_, text, text_color, renderer);
-    }
-}
-
-void TextView::SetPos(Vector2 pos) { view_info_.pos = pos; }
-
-void TextView::SetSize(Vector2 size) { view_info_.size = size; }
-
-void TextView::SetHViewType(ViewType view_type) { view_info_.h_view_type = view_type; }
-
-void TextView::SetVViewType(ViewType view_type) { view_info_.v_view_type = view_type; }
-
-void TextView::SetAnchor(Anchor anchor) { view_info_.anchor = anchor; }
-
-void TextView::SetPivot(Pivot pivot) { view_info_.pivot = pivot; }
-
-void TextView::Render(SDL_Renderer* renderer) {
-    if (window_ == NULL) return;
-
-    if (texture_) {
-        float pos_x = view_info_.pos.x;
-        float pos_y = view_info_.pos.y;
-        float size_w = view_info_.size.x;
-        float size_h = view_info_.size.y;
-
-        switch (view_info_.h_view_type) {
-        case kWrapContent:
-            size_w = texture_->get_width();
-        }
-
-        switch (view_info_.v_view_type) {
-        case kWrapContent:
-            size_h = texture_->get_height();
-        }
-
-        /**
-         * アンカーの座標を計算
-         */
-        float anchor_pos_x = 0.f;
-        float anchor_pos_y = 0.f;
-
-        int anchor_h = view_info_.anchor & MASK_V;
-        if (anchor_h == ANCHOR_PIVOT_H_CENTER) {
-            anchor_pos_x = window_->get_width() / 2.f;
-        } else if (anchor_h == ANCHOR_PIVOT_RIGHT) {
-            anchor_pos_x = (float)window_->get_width();
-        }
-
-        int anchor_v = view_info_.anchor & MASK_H;
-        if (anchor_v == ANCHOR_PIVOT_V_CENTER) {
-            anchor_pos_y = window_->get_height() / 2.f;
-        } else if (anchor_v == ANCHOR_PIVOT_BOTTOM) {
-            anchor_pos_y = (float)window_->get_height();
-        }
-
-        /**
-         * ビューの座標を計算
-         */
-        float pivot_pos_x = anchor_pos_x + pos_x;
-        float pivot_pos_y = anchor_pos_y + pos_y;
-
-        int pivot_h = view_info_.pivot & MASK_V;
-        if (pivot_h == ANCHOR_PIVOT_H_CENTER) {
-            pos_x = pivot_pos_x - (size_w / 2.f);
-        } else if (pivot_h == ANCHOR_PIVOT_RIGHT) {
-            pos_x = pivot_pos_x - size_w;
-        } else {
-            pos_x = pivot_pos_x;
-        }
-
-        int pivot_v = view_info_.pivot & MASK_H;
-        if (pivot_v == ANCHOR_PIVOT_V_CENTER) {
-            pos_y = pivot_pos_y - (size_h / 2.f);
-        } else if (pivot_v == ANCHOR_PIVOT_BOTTOM) {
-            pos_y = pivot_pos_y - size_h;
-        } else {
-            pos_y = pivot_pos_y;
-        }
-
-        SDL_FRect rect{pos_x, pos_y, size_w, size_h};
-        SDL_RenderCopyF(renderer, texture_->sdl(), NULL, &rect);
     }
 }
 
--- spaceshooter/src/gui/text_view.h
+++ spaceshooter/src/gui/text_view.h
@@ -2,74 +2,23 @@
 
 #include <SDL.h>
 
-#include "../common/vector2.h"
 #include "../font/font.h"
-#include "../texture/texture.h"
-#include "../window.h"
+#include "view.h"
 
 namespace spaceshooter {
 
-const int ANCHOR_PIVOT_TOP = 0b0100;
-const int ANCHOR_PIVOT_BOTTOM = 0b1000;
-const int ANCHOR_PIVOT_LEFT = 0b0001;
-const int ANCHOR_PIVOT_RIGHT = 0b0010;
-
-const int ANCHOR_PIVOT_V_CENTER = 0b1100;
-const int ANCHOR_PIVOT_H_CENTER = 0b0011;
-
-const int MASK_V = 0b0011;
-const int MASK_H = 0b1100;
-
-enum Anchor {
-    kTopLeft = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_LEFT,
-    kTopRight = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_RIGHT,
-    kTopCenter = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_LEFT | ANCHOR_PIVOT_RIGHT,
-    kCenterLeft = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_LEFT,
-    kCenterRight = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_RIGHT,
-    kCenterCenter = ANCHOR_PIVOT_TOP | ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_LEFT | ANCHOR_PIVOT_RIGHT,
-    kBottomLeft = ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_LEFT,
-    kBottomRight = ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_RIGHT,
-    kBottomCenter = ANCHOR_PIVOT_BOTTOM | ANCHOR_PIVOT_LEFT | ANCHOR_PIVOT_RIGHT
-};
-
-using Pivot = Anchor;
-
-enum ViewType { kFill, kWrapContent };
-
-struct ViewInfo {
-    Vector2 pos;
-    Vector2 size;
-    ViewType h_view_type;
-    ViewType v_view_type;
-    Pivot pivot;
-    Anchor anchor;
-};
-
-class TextView {
+class TextView : public View {
  public:
     TextView();
-    TextView(Window* window, ViewInfo view_info, std::string font_path = "", int point_size = 0);
 
     ~TextView();
 
     void set_font(std::string, int point_size);
-    void set_window(Window* window);
 
     void SetText(std::string text, SDL_Color text_color, SDL_Renderer* renderer);
-    void SetPos(Vector2 pos);
-    void SetSize(Vector2 size);
-    void SetHViewType(ViewType view_type);
-    void SetVViewType(ViewType view_type);
-    void SetAnchor(Anchor anchor);
-    void SetPivot(Pivot pivot);
-
-    void Render(SDL_Renderer* renderer);
 
  private:
-    ViewInfo view_info_;
     Font* font_;
-    Texture* texture_;
-    Window* window_;
 };
 
 } // namespace spaceshooter
